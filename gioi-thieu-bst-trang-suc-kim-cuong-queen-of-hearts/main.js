f63617f3d64a5c22092a9649b578f2a8184368a7
Nhim <thanhhung.nt93@gmail.com>
2023-12-15 18:24:07 +0700

    -fix gioi-thieu-bst-trang-suc-kim-cuong-queen-of-hear

--- gioi-thieu-bst-trang-suc-kim-cuong-queen-of-hearts/main.js
+++ gioi-thieu-bst-trang-suc-kim-cuong-queen-of-hearts/main.js
@@ -4,13 +4,13 @@ $(function(){
       'height': $('.lp-content_bottom-content-2').find('.right').find('iframe').width() * 480 / 853
     });
   
-    wow = new WOW({
-      boxClass:     'wow',      // default
-      animateClass: 'animated', // default
-      offset: 50,
-      mobile:       true,       // default
-      live:         true        // default
-    })
-    wow.init();
+    // wow = new WOW({
+    //   boxClass:     'wow',      // default
+    //   animateClass: 'animated', // default
+    //   offset: 50,
+    //   mobile:       true,       // default
+    //   live:         true        // default
+    // })
+    // wow.init();
   
   });
